1bfdead41ec65094478ed4042fb8ffcf20f9967f
Shlomo <sh.z.wind@gmail.com>
2025-09-08 10:10:20 +0300

    After I did the logic in one file, I was able to pull from Kafka and publish to Elasticsearch and MongoDB as required, now I need to split it into separate files

--- database-distribution/consumer.py
+++ database-distribution/consumer.py
@@ -1,12 +1,15 @@
 import json
+from mongo_inserter import MongoConnector,WavInserter
 from elasticsearch import Elasticsearch
 from create_unique_id import UniqueId
 from kafka import KafkaConsumer
-from config.environments import KAFKA_ADDRESS,FIRST_JSON_TOPIC,ELASTICSEARCH
+from config.environments import KAFKA_ADDRESS,FIRST_JSON_TOPIC,ELASTICSEARCH,MONGO_URL,MONGO_DB_NAME,COLLECTION_NAME
 
 
 class Consumer:
     def __init__(self,topic,server_address):
+        self.mongo_connector = MongoConnector(MONGO_URL,MONGO_DB_NAME,COLLECTION_NAME)
+        self.mongo_inserter = WavInserter(self.mongo_connector)
         self.es = Elasticsearch(ELASTICSEARCH)
         self.unique_id = UniqueId
         self.topic = topic
@@ -14,7 +17,8 @@ class Consumer:
         self.consumer = KafkaConsumer(self.topic,
                                       bootstrap_servers=[self.server_address],
                                       value_deserializer=lambda v: json.loads(v.decode("utf-8")))
-                                      # consumer_timeout_ms=10000)
+
+
 
     def attach_id(self):
         data_for_es = {}
@@ -29,8 +33,9 @@ class Consumer:
             data_for_es["metadata"] = message.value["metadata"]
             print(f"data for elastic {data_for_es}")
             self.es.index(index="test", document=data_for_es)
-            print("inserted")
-
+            print("inserted_to_elastic")
+            # print(message.value['file path'])
+            self.mongo_inserter.read_wav(message.value['file path'],uniq_id)
 
 
 d = Consumer(FIRST_JSON_TOPIC,KAFKA_ADDRESS)
---
+++ database-distribution/mongo_inserter.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+from bson.binary import Binary
+
+
+class MongoConnector:
+    def __init__(self,url,db_name,collection_name):
+        self.client = MongoClient(url)
+        self.db = self.client[db_name]
+        self.coll = self.db[collection_name]
+
+class WavInserter:
+    def __init__(self,mongo_connection:MongoConnector):
+        self.conn = mongo_connection
+
+    def read_wav(self,file_path,unique_id):
+        with open(file_path, 'rb') as f:
+            wav_data = f.read()
+            document = {
+                "unique_id": unique_id,
+                "content_type": "audio/wav",
+                "wav_file": Binary(wav_data)
+            }
+            result = self.conn.coll.insert_one(document)
+            print(f"Document inserted with ID: {result.inserted_id}")
