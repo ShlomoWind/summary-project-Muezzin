d196949b442374ccc13e9a6f50628de5399fb0d4
Shlomo <sh.z.wind@gmail.com>
2025-09-09 10:35:13 +0300

    started dividing the code into classes

--- audio-transcription/binary_to_wav.py
+++ audio-transcription/binary_to_wav.py
@@ -1,14 +1,21 @@
-# class BinaryConvert:
-#     def __init__(self,file_name,binary_data):
-#         self.file_name = file_name
-#         self.binary_data = binary_data
-#
-#     def write_to_wav(self):
-#         with open(f"{self.file_name}.wav", "wb") as f:
-#             f.write(self.binary_data)
-#             return f
-#
-#
-#     document = collection.find_one({'_id': 'your_document_id'})
-#     if document and 'audio_data' in document:
-#         wav_binary_data = document['audio_data']
+import speech_recognition as sr
+from utils.class_logger import Logger
+
+class BinaryTranscript:
+    def __init__(self,binary_data,unique_id):
+        self.binary_data = binary_data
+        self.unique_id = unique_id
+        self.logger = Logger.get_logger()
+
+    def transcript(self):
+        result = {'unique_id': self.unique_id}
+        output_filename = 'temp_audio.wav'
+        with open(output_filename, 'wb') as f:
+            f.write(self.binary_data)
+        self.logger.info(f"WAV file saved as {output_filename}")
+        r = sr.Recognizer()
+        with sr.AudioFile(output_filename) as source:
+            audio = r.record(source)
+            transcription = r.recognize_google(audio)
+            result['transcription'] = transcription
+        return result
